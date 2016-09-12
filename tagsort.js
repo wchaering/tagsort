564173db96fe03c3248460e38e8609e5c1dfce61
Will Haering <wchaering@gmail.com>
2016-09-11 23:42:49 -0700

    Improved tag handling
    Added:
    - Option to pre-select tags
    - Option to control the actual element that is hidden/shown (useful if
    tagged elements have parents)

--- tagsort.js
+++ tagsort.js
@@ -4,6 +4,7 @@
     var defaults = {
       items: '.item-tagsort',
       tagElement: 'span',
+      selectedTags: [],
       tagClassPrefix: false,
       itemTagsView: false,
       itemTagsSeperator: ' ',
@@ -11,53 +12,58 @@
       sortType: 'exclusive',
       fadeTime: 0,
       sortAlphabetical: false,
-      reset: false
+      reset: false,
+      getElement: function(element){ return element; },
     };
     // Overwrite defaults with any user-supplied options
     options = $.extend(defaults, options);
     // Namespace
     var tagSortEngine = {
-      generateTags: function(elements) {
+      generateTags: function() {
         var tags_inclusive = {},
             tags_exclusive = {pointers: [], tags: []},
             tagElement = $(document.createElement(options.tagElement));
         // Loop through tagged elements
-        elements.each(function(i) {
+        tagSortEngine.container.html('');
+        tagSortEngine.elements.each(function(i) {
           $element = $(this);
           // Pull tags from element data attribute and dump into array
-          var tagsData = $element.data('item-tags');
-          
-          if (typeof tagsData === 'string' && tagsData !== '') {
-            var elementTags = tagsData.match(/,\s+/) ? tagsData.split(', ') : tagsData.split(',');
-              // Inclusive Filtering only: Loop through each element's tags
-              $.each(elementTags, function(key, v) {
-                var tagName = v.toLowerCase();
-                // Move tags to object as property names if they don't already exist
-                // Set property value to empty array
-                if (!tags_inclusive[tagName]) {
-                  tags_inclusive[tagName] = [];
-                  //Add tag name as class to each tag element with optional prefix if the user sets tagClass = true
-                  tagSortEngine.container.append(options.tagClassPrefix !== false ? tagElement.clone().text(v).addClass((options.tagClassPrefix + v.toLowerCase()).replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')) : tagElement.clone().text(v));
+          var tagsData = $element.attr('data-item-tags')
+          tagsData = (typeof tagsData === 'string' && tagsData !== '') ?  tagsData : 'Untagged';
+          var elementTags = tagsData.match(/,\s+/) ? tagsData.split(', ') : tagsData.split(',');
+            // Inclusive Filtering only: Loop through each element's tags
+            $.each(elementTags, function(key, v) {
+              var tagName = v;
+              var tagPropertyName = tagName.toLowerCase();
+              // Move tags to object as property names if they don't already exist
+              // Set property value to empty array
+              if (!tags_inclusive[tagPropertyName]) {
+                tags_inclusive[tagPropertyName] = [];
+                //Add tag name as class to each tag element with optional prefix if the user sets tagClass = true
+                var tag = (options.tagClassPrefix !== false) ? tagElement.clone().text(tagName).addClass((options.tagClassPrefix + v.toLowerCase()).replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '')) : tagElement.clone().text(tagName);
+                if(options.selectedTags.includes(tagName))
+                  tag.addClass('active');
+                tagSortEngine.container.append(tag);
+              }
+              // Append tags to the element if they should be displayed
+              if (options.itemTagsView !== false) {
+                if (options.itemTagsElement !== false) {
+                  $element.find(options.itemTagsView).append($(document.createElement(options.itemTagsElement)).clone().text(v));
                 }
-                // Append tags to the element if they should be displayed
-                if (options.itemTagsView !== false) {
-                  if (options.itemTagsElement !== false) {
-                    $element.find(options.itemTagsView).append($(document.createElement(options.itemTagsElement)).clone().text(v));
-                  }
-                  else {
-                    $element.find(options.itemTagsView).append(key > 0 ? options.itemTagsSeperator + v : v);
-                  }
+                else {
+                  $element.find(options.itemTagsView).append(key > 0 ? options.itemTagsSeperator + v : v);
                 }
-                // Push elements to array for each tag they have
-                tags_inclusive[tagName].push(i);
-              });
-            // Exclusive Filtering: Push elements to array and push array
-            // of the element's tags to second array (index of element and its tags match)
-            if (options.sortType == 'exclusive') {
-              tags_exclusive.pointers.push(i);
-              tags_exclusive.tags.push(elementTags);
-            }
+              }
+              // Push elements to array for each tag they have
+              tags_inclusive[tagPropertyName].push(i);
+            });
+          // Exclusive Filtering: Push elements to array and push array
+          // of the element's tags to second array (index of element and its tags match)
+          if (options.sortType == 'exclusive') {
+            tags_exclusive.pointers.push(i);
+            tags_exclusive.tags.push(elementTags);
           }
+
         });
         // Return proper object based on filter type
         if (options.sortType == 'inclusive' || options.sortType == 'single') {
@@ -102,33 +108,37 @@
       showElements: function(pointers, elements) {
         // Fade in elements in display[1]
         $.each(pointers, function(key, pointer) {
-            elements.eq(pointer).fadeIn(options.fadeTime);
+            element = elements.eq(pointer);
+            element = options.getElement(element);
+            element.fadeIn(options.fadeTime);
         });
       },
       hideElements: function(pointers, elements) {
         // Fade out elements in display[0]
         $.each(pointers, function(key, pointer) {
-            elements.eq(pointer).fadeOut(options.fadeTime);
+          element = elements.eq(pointer);
+          element = options.getElement(element);
+          element.fadeOut(options.fadeTime);
         });
       },
       inititalize: function(tagsContainer) {
         tagSortEngine.container = tagsContainer;
-        var elements = $(options.items),
-            pointers = [],
-            display,
+        tagSortEngine.elements = $(options.items);
+        tagSortEngine.pointers = [];
+        var tdisplay,
             reset = options.reset;
         // Create array of pointers to represent elements
-        for(var i = 0; i < elements.length; i++) {
-          pointers.push(i);
+        for(var i = 0; i < tagSortEngine.elements.length; i++) {
+          tagSortEngine.pointers.push(i);
         }
         // Generate tags from element data-attributes
-        tagSortEngine.tags = tagSortEngine.generateTags(elements, tagSortEngine.container);
+        tagSortEngine.tags = tagSortEngine.generateTags(tagSortEngine.lements, tagSortEngine.container);
         // Get all clickable tag elements
-        var tagElement = tagSortEngine.container.find(options.tagElement);
+        tagSortEngine.tagElements = tagSortEngine.container.find(options.tagElement);
 
         if (options.sortAlphabetical) {
             // Sort values.
-            var sortedTags = tagElement.toArray().sort(function(a, b) {
+            var sortedTags = tagSortEngine.tagElements.toArray().sort(function(a, b) {
                 return a.innerText > b.innerText;
             });
 
@@ -140,9 +150,10 @@
                 tagSortEngine.container.append(value);
             });
         }
+        tagSortEngine.sort();
 
         // Handle tag click based on user options
-        tagElement.click(function() {
+        tagSortEngine.tagElements.click(function() {
           // Handle single filtering (inclusive sort run one tag at a time)
           if (options.sortType == 'single') {
             if ($(this).hasClass('active')) {
@@ -151,19 +162,13 @@
             else {
               $('.active').removeClass('active');
               $(this).toggleClass('active');
-              display = tagSortEngine.inclusiveSort(tagSortEngine.tags, pointers.slice());
             }
           }
           // Handle inclusive or exclusive filtering
           else {
             $(this).toggleClass('active');
-            display = (options.sortType == 'inclusive') ? tagSortEngine.inclusiveSort(tagSortEngine.tags, pointers.slice()) : tagSortEngine.exclusiveSort(tagSortEngine.tags);
           }
-          // Show all elements if no tags are selected
-          if (!tagElement.hasClass('active')) display = [[],pointers.slice()];
-          // Show/hide tagged elements
-          if (display[0].length > 0) tagSortEngine.hideElements(display[0], elements);
-          if (display[1].length > 0) tagSortEngine.showElements(display[1], elements);
+          tagSortEngine.sort();
         });
         if (reset) {
           $(reset).click(function() {
@@ -172,6 +177,21 @@
             tagSortEngine.showElements(display[1], elements);
           });
         }
+      },
+      sort: function(){
+        // Handle single filtering (inclusive sort run one tag at a time)
+        if (options.sortType == 'single') {
+            display = tagSortEngine.inclusiveSort(tagSortEngine.tags, pointers.slice());
+        }
+        // Handle inclusive or exclusive filtering
+        else {
+          display = (options.sortType == 'inclusive') ? tagSortEngine.inclusiveSort(tagSortEngine.tags, pointers.slice()) : tagSortEngine.exclusiveSort(tagSortEngine.tags);
+        }
+        // Show all elements if no tags are selected
+        if (!tagSortEngine.tagElements.hasClass('active')) display = [[], tagSortEngine.pointers.slice()];
+        // Show/hide tagged elements
+        if (display[0].length > 0) tagSortEngine.hideElements(display[0], tagSortEngine.elements);
+        if (display[1].length > 0) tagSortEngine.showElements(display[1], tagSortEngine.elements);
       }
     }
     // Start it up
